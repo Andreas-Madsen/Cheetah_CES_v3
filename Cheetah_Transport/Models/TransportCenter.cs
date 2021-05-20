ceccc0838d00feddded08ebd4b40eecb2b2bd8aa
Frederik Johansen <frederik.lynggaard@icloud.com>
2021-05-20 15:36:07 +0200

    Initial model commit

---
+++ Cheetah_Transport/Models/TransportCenter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cheetah_Transport.Models
+{
+    public class TransportCenter
+    {
+        public int Id { get; set; }
+
+    }
+}
