1d53bf01c973637a67d92f69aababed31332aadf
NCLAN\afw <afw@netcompany.com>
2021-05-20 23:30:27 +0200

    implement frontend further

---
+++ Cheetah_Transport/Controllers/SummaryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cheetah_Transport.Controllers
+{
+    public class SummaryController : Controller
+    {
+        // GET: Summary
+        public ActionResult SummaryPage()
+        {
+            return View();
+        }
+    }
+}
